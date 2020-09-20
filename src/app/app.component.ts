78da30b14bc9e2426640558ed7fef48c18a575e5
DevNand0 <fernandoo.avellaneda.m@gmail.com>
2020-09-20 01:38:00 -0500

    validcion de inputs en formulario, textos requeridos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'tdf/user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   topics = ['Angular','React','Vue'];
+  userModel = new User("Fernanda Inayao","fernanditaaaa1992@gmail.com",5334521,"","morning",true);
 }
