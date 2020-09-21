40ea60e9d765cc174156e2bc23217cf8a3e292e0
DevNand0 <fernandoo.avellaneda.m@gmail.com>
2020-09-21 02:43:33 -0500

    validaciones de formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from 'tdf/user';
+import { EnrollmentService } from 'tdf/enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,23 @@ import { User } from 'tdf/user';
 export class AppComponent {
   topics = ['Angular','React','Vue'];
   userModel = new User("Fernanda Inayao","fernanditaaaa1992@gmail.com",5334521,"","morning",true);
+
+  constructor(private _enrollmentService: EnrollmentService){}
+
+  topicHasError=true;
+  validateTopic(value){
+    if(value===""){
+      this.topicHasError=true;
+    }else{
+      this.topicHasError=false;
+    }
+  }
+
+  onSubmit(){
+    
+    this._enrollmentService.enroll(this.userModel).subscribe(
+      data=>console.log("Success",data),
+      error=>console.log("Error",error)
+    )
+  }
 }
